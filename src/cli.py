bd30ad0d31507594283555bfb0e8baba53904514
Federico Mantovani <fettemanto@gmail.com>
2025-04-09 16:28:19 +0200

    Added visualization script

--- src/cli.py
+++ src/cli.py
@@ -1,12 +1,16 @@
-import argparse
+import datetime
 import sys
-from pathlib import Path
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace, _SubParsersAction
+
+from matplotlib import pyplot as plt
+from matplotlib.pylab import pareto
 
 from download import fetch_gdf, get_dates
 from utils import DEFAULT_DATES_FILE, DEFAULT_GDF_URL
+from visualization import plot_daily_radiance
 
 
-def handle_bm_commands(args: argparse.Namespace):
+def handle_bm_commands(args: Namespace):
     if args.bm_command == "download":
         from download import bm_dataset_preprocess
 
@@ -20,18 +24,29 @@ def handle_bm_commands(args: argparse.Namespace):
         bm_dataset_preprocess(gdf=gdf, dates=dates, force=args.force)
         print("Download done.")
 
+    if args.bm_command == "show":
+        from download import bm_dataset_preprocess
 
-def main():
-    # Main argument parser
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    subparsers = parser.add_subparsers(dest="command")
+        gdf = fetch_gdf(args.gdf)
+        date = datetime.date.fromisoformat(args.date)
+        raster = bm_dataset_preprocess(gdf=gdf, dates=[date])
+        figure = plot_daily_radiance(gdf, raster, date)
+        plt.show(block=True)
 
-    # Blackmarble parser
-    bm_parser = subparsers.add_parser("bm", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    bm_subparsers = bm_parser.add_subparsers(dest="bm_command")
 
-    bm_download_parser = bm_subparsers.add_parser(
-        "download", formatter_class=argparse.ArgumentDefaultsHelpFormatter
+def setup_bm_parser(parser: ArgumentParser, parents: list[ArgumentParser]):
+    bm_parent = ArgumentParser(add_help=False)
+    bm_parent.add_argument("-g", "--gdf", default=DEFAULT_GDF_URL, help="URL to the GDF JSON file")
+
+    subparsers_bm = parser.add_subparsers(dest="bm_command")
+
+    # Download
+    bm_download_parser = subparsers_bm.add_parser(
+        "download",
+        add_help=False,
+        parents=parents + [bm_parent],
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        help="Download the Blackmarble dataset locally",
     )
     bm_download_parser.add_argument(
         "-f",
@@ -43,16 +58,58 @@ def main():
         "-d", "--dates", nargs="+", help="Manually list dates to download in ISO format"
     )
     bm_download_parser.add_argument(
-        "-g", "--gdf", default=DEFAULT_GDF_URL, help="URL to the GDF JSON file"
-    )
-    bm_download_parser.add_argument(
         "--force", action="store_true", help="Force re-downloading and processing of files"
     )
 
-    # ... Add more subparsers here
+    # Visualize
+    bm_show_parser = subparsers_bm.add_parser(
+        "show",
+        add_help=False,
+        parents=parents + [bm_parent],
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        help="Visualize data in the Blackmarble dataset",
+    )
+    bm_show_parser.add_argument("date", help="Date to visualize")
 
-    # General options
-    parser.add_argument("-v", "--verbose", action="store_true", help="More verbose logging")
+
+def setup_lj_parser(parser: ArgumentParser, parents: list[ArgumentParser]):
+    pass
+
+
+def main():
+    # Root parent parser
+    root_parent = ArgumentParser()
+    root_parent.add_argument(
+        "-v", "--verbose", action="store_true", help="Print debugging information"
+    )
+
+    # Main argument parser
+    parser = ArgumentParser(
+        description="NTL Tool", prog="ntl-tool", formatter_class=ArgumentDefaultsHelpFormatter
+    )
+    subparsers = parser.add_subparsers(title="Commands", dest="command", help="Subcommand to run")
+
+    # Blackmarble parser
+    parser_bm = subparsers.add_parser(
+        "bm",
+        parents=[root_parent],
+        add_help=False,
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        help="Commands relating to the Blackmarble dataset",
+    )
+    setup_bm_parser(parser_bm, parents=[root_parent])
+
+    # Luojia parser
+    parser_lj = subparsers.add_parser(
+        "lj",
+        parents=[parser],
+        add_help=False,
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        help="Commands relating to the Luojia dataset",
+    )
+    setup_lj_parser(parser_lj, parents=[root_parent])
+
+    # ... Add more subparsers here
 
     # Main program
     args = parser.parse_args()

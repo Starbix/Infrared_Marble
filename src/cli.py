8a16b3847b48e4c11d6118a6ea526286b6c972d4
Federico Mantovani <fettemanto@gmail.com>
2025-04-09 15:29:11 +0200

    Update download CLI code

--- src/cli.py
+++ src/cli.py
@@ -2,20 +2,23 @@ import argparse
 import sys
 from pathlib import Path
 
+from download import fetch_gdf, get_dates
 from utils import DEFAULT_DATES_FILE, DEFAULT_GDF_URL
 
 
 def handle_bm_commands(args: argparse.Namespace):
     if args.bm_command == "download":
-        from download import bm_download_all
+        from download import bm_dataset_preprocess
 
         if args.file:
-            path = Path(args.file)
-            dates = path.read_text(encoding="utf-8").splitlines()
+            dates = get_dates(args.file)
         else:
             dates = args.dates
-
-        bm_download_all(gdf_url=args.gdf, dates=dates)
+        print("Selected GDF:    ", args.gdf.split("/")[-1])
+        gdf = fetch_gdf(args.gdf, force=args.force)
+        print("Selected dates:", ", ".join(d.isoformat() for d in dates))
+        bm_dataset_preprocess(gdf=gdf, dates=dates, force=args.force)
+        print("Download done.")
 
 
 def main():
@@ -42,6 +45,9 @@ def main():
     bm_download_parser.add_argument(
         "-g", "--gdf", default=DEFAULT_GDF_URL, help="URL to the GDF JSON file"
     )
+    bm_download_parser.add_argument(
+        "--force", action="store_true", help="Force re-downloading and processing of files"
+    )
 
     # ... Add more subparsers here
 
@@ -50,8 +56,6 @@ def main():
 
     # Main program
     args = parser.parse_args()
-
-    print(args)
 
     try:
         if args.command == "bm":

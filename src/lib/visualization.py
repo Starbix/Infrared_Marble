865d61cf78bdbfa795c103d3831ede35bfc36be7
Federico Mantovani <fettemanto@gmail.com>
2025-04-09 21:56:13 +0200

    Added difference visualization for BM dataset

--- src/lib/visualization.py
+++ src/lib/visualization.py
@@ -11,10 +11,15 @@ if TYPE_CHECKING:
     from geopandas import GeoDataFrame
 
 
-def plot_daily_radiance(gdf: "GeoDataFrame", raster: "xr.Dataset", date: datetime.date | str):
+def get_subplots(raster: "xr.Dataset", figwidth: float = 8.0):
     ratio = raster.y.shape[0] / raster.x.shape[0]
-    figwidth = 8
     fig, ax = plt.subplots(figsize=(figwidth, ratio * figwidth))
+
+    return fig, ax
+
+
+def plot_daily_radiance(gdf: "GeoDataFrame", raster: "xr.Dataset", date: datetime.date | str):
+    fig, ax = get_subplots(raster)
 
     if not isinstance(date, str):
         date = date.isoformat()
@@ -41,3 +46,33 @@ def plot_daily_radiance(gdf: "GeoDataFrame", raster: "xr.Dataset", date: datetim
     ax.set_title(f"Myanmar: NTL Radiance on {date}", fontsize=20)
 
     return fig
+
+
+def plot_difference(
+    gdf: "GeoDataFrame",
+    raster: "xr.Dataset",
+    date1: datetime.date | str,
+    date2: datetime.date | str,
+):
+    date1 = date1 if isinstance(date1, str) else date1.isoformat()
+    date2 = date2 if isinstance(date2, str) else date2.isoformat()
+
+    data = raster["Gap_Filled_DNB_BRDF-Corrected_NTL"]
+    delta = (data.sel(time=date2) - data.sel(time=date1)) / data.sel(time=date1)
+
+    fig, ax = get_subplots(raster)
+    delta.plot.pcolormesh(ax=ax, cmap="Spectral", robust=True)
+    cx.add_basemap(ax, crs=gdf.crs.to_string(), source=cx.providers.CartoDB.DarkMatter)
+
+    ax.text(
+        0,
+        -0.1,
+        "Source: NASA Black Marble VNP46A3",
+        ha="left",
+        va="center",
+        transform=ax.transAxes,
+        fontsize=10,
+        color="black",
+        weight="normal",
+    )
+    ax.set_title("Ghana: NTL Radiance Increase/Decrease (2019-2022)", fontsize=16)

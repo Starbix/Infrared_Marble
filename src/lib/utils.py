f28285c3d252c2a5f179b031702874d04e9db0fd
Federico Mantovani <fettemanto@gmail.com>
2025-04-10 16:48:29 +0200

    Add blackmarble product to utils

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -2,6 +2,8 @@ import os
 import sys
 from pathlib import Path
 
+from blackmarble.types import Product
+
 # Note: This code runs whenever the module is first imported.
 # This is to make sure that the DATA_DIR and BEARER_TOKEN variables are appropriately set.
 
@@ -15,10 +17,10 @@ else:
 
 # Derived constants
 BM_DATA_DIR = DATA_DIR / "blackmarble"
-LUOJIA_DATA_DIR = DATA_DIR / "luojia"
+LJ_DATA_DIR = DATA_DIR / "luojia"
 
 # Ensure data dirs exists
-for _d in [DATA_DIR, BM_DATA_DIR, LUOJIA_DATA_DIR]:
+for _d in [DATA_DIR, BM_DATA_DIR, LJ_DATA_DIR]:
     _d.mkdir(parents=True, exist_ok=True)
 
 # Get Bearer token from environment
@@ -33,6 +35,10 @@ if not _bearer_token_from_env or len(_bearer_token_from_env) == 0:
 
 BEARER_TOKEN = _bearer_token_from_env
 
+BM_PRODUCT = Product.VNP46A2
+BM_VARIABLE = None  # Default varible for product
+BM_QUALITY_FLAG = [2]  # Drop low-quality pixels
 
 DEFAULT_DATES_FILE = PROJECT_ROOT / "dates_luojia_myanmar.csv"
 DEFAULT_GDF_URL = "https://geodata.ucdavis.edu/gadm/gadm4.1/json/gadm41_MMR_1.json.zip"
+DEFAULT_LJ_IDS_FILE = PROJECT_ROOT / "luojia_image_ids.csv"

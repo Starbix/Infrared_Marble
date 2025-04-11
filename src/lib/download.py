4e86b6b61a3a0e22fc53fbe5c85af2e9a9cdfae7
Cédric Laubacher <cedric@laubacher.io>
2025-04-11 13:31:42 +0200

    Initial LJ tile infrastructure

--- src/lib/download.py
+++ src/lib/download.py
@@ -1,17 +1,16 @@
 import datetime
 import tarfile
-from concurrent.futures import ThreadPoolExecutor, as_completed
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 import pandas as pd
 import geopandas
-import httpx
 import requests
+import tarfile
 import xarray as xr
 from blackmarble.raster import bm_raster
 from httpx import HTTPError
-from tqdm import tqdm
 
 from lib.loading import get_bm
 from lib.utils import (
@@ -21,7 +20,6 @@ from lib.utils import (
     BM_QUALITY_FLAG,
     BM_VARIABLE,
     DATA_DIR,
-    LJ_DATA_DIR,
 )
 
 if TYPE_CHECKING:
@@ -108,3 +106,63 @@ def bm_dataset_preprocess(
 
     # Loads fresh from disk
     return get_bm(zarr_path)
+
+
+## LUOJIA
+
+METADATA_URL = "https://polybox.ethz.ch/index.php/s/dnP82nHZkjR4gr7/download/file?path=%2Fmetadata%2FMETA.tar.gz"
+
+def luojia_metadata(metadata_url: str):
+    # download it, if it doesn't exist
+    # unpack it
+    metadata_path = DATA_DIR / "luojia" / "metadata"
+    metadata_path.mkdir(parents=True, exist_ok=True)
+    metadata_file = metadata_path / "META.tar.gz"
+    if not metadata_file.exists():
+        print(f"Downloading to {metadata_file}")
+        response = requests.get(metadata_url, timeout=30.0)
+        if response.status_code >= 400:
+            raise HTTPError("Failed to download metadata. Please double-check the URL and try again.")
+        metadata_file.write_bytes(response.content)
+        print("Metadata successfully downloaded")
+    else:
+        print("Metadata already downloaded, skipping... (use --force to re-download)")
+
+    # Unpack the tar.gz file
+    with tarfile.open(metadata_file, "r:gz") as tar:
+        tar.extractall(path=metadata_path)
+        print("Metadata successfully extracted")
+
+TILE_URL = "https://polybox.ethz.ch/index.php/s/dnP82nHZkjR4gr7/download/file?path=%2F"
+def luojia_tile_download(tile_name: str):
+    tile_url = TILE_URL + tile_name + ".tar.gz"
+    # download it, if it doesn't exist
+    # unpack it
+    tile_path = DATA_DIR / "luojia" / "tiles"
+    tile_path.mkdir(parents=True, exist_ok=True)
+
+    # Check if any file in the directory contains the tile_name
+    if any(tile_name in file.name for file in tile_path.iterdir()):
+        print(f"Tile {tile_name} already exists, skipping download...")
+        return
+
+    tile_file = tile_path / (tile_name + ".tar.gz")
+    response = requests.get(tile_url, timeout=30.0)
+    if response.status_code >= 400:
+        raise HTTPError("Failed to download tile. Please double-check the URL and try again.")
+    tile_file.write_bytes(response.content)
+
+    # Unpack the tar.gz file
+    with tarfile.open(tile_file, "r:gz") as tar:
+        tar.extractall(path=tile_path)
+        print("Tile successfully extracted")
+
+    # Delete the tar.gz file after extraction
+    tile_file.unlink()
+
+
+if __name__ == "__main__":
+
+    luojia_metadata(METADATA_URL)
+    tile_name = "LuoJia1-01_LR201806057936_20180603055109_HDR_0029"
+    luojia_tile_download(tile_name)

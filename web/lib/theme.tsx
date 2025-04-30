807731920280f42a61139227afb1c88b5852bba6
Federico Mantovani <fettemanto@gmail.com>
2025-04-30 17:54:13 +0200

    Added layout and navigation

--- web/lib/theme.tsx
+++ web/lib/theme.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { createTheme } from "@mui/material";
+import Link from "next/link";
 
 const theme = createTheme({
   typography: {
@@ -8,14 +9,55 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      main: "#3d69b9",
-      light: "#4a8cde",
-      dark: "#324b99",
+      main: "#2c2d30",
+      light: "#6e6f72",
+      dark: "#000",
+      contrastText: "#fff",
     },
     secondary: {
-      main: "#b98d3d",
-      light: "#c4b64f",
-      dark: "#ad6d2d",
+      main: "#4e4ec2",
+      light: "#8185d6",
+      dark: "#3f3bac",
+      contrastText: "#fff",
+    },
+  },
+  shadows: [
+    "none",
+    "0px 0px 2px rgba(0,0,0,0.06)",
+    "0px 0px 2px rgba(0,0,0,0.06)",
+    "0px 0px 3px rgba(0,0,0,0.07)",
+    "0px 0px 4px rgba(0,0,0,0.07)",
+    "0px 0px 5px rgba(0,0,0,0.07)",
+    "0px 0px 6px rgba(0,0,0,0.08)",
+    "0px 0px 7px rgba(0,0,0,0.08)",
+    "0px 0px 8px rgba(0,0,0,0.08)",
+    "0px 0px 9px rgba(0,0,0,0.09)",
+    "0px 0px 10px rgba(0,0,0,0.09)",
+    "0px 0px 11px rgba(0,0,0,0.09)",
+    "0px 0px 12px rgba(0,0,0,0.10)",
+    "0px 0px 13px rgba(0,0,0,0.10)",
+    "0px 0px 14px rgba(0,0,0,0.11)",
+    "0px 0px 15px rgba(0,0,0,0.11)",
+    "0px 0px 16px rgba(0,0,0,0.11)",
+    "0px 0px 17px rgba(0,0,0,0.12)",
+    "0px 0px 18px rgba(0,0,0,0.12)",
+    "0px 0px 19px rgba(0,0,0,0.12)",
+    "0px 0px 20px rgba(0,0,0,0.13)",
+    "0px 0px 21px rgba(0,0,0,0.13)",
+    "0px 0px 22px rgba(0,0,0,0.13)",
+    "0px 0px 23px rgba(0,0,0,0.14)",
+    "0px 0px 24px rgba(0,0,0,0.14)",
+  ],
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: Link,
+      },
+    },
+    MuiLink: {
+      defaultProps: {
+        component: Link,
+      },
     },
   },
 });

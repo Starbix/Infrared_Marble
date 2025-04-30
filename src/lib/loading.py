f814f21419ca27a0f4b025d5b6feb4a1986700b7
Federico Mantovani <fettemanto@gmail.com>
2025-04-30 12:04:39 +0200

    BM download changes

--- src/lib/loading.py
+++ src/lib/loading.py
@@ -2,11 +2,11 @@ from pathlib import Path
 
 import xarray
 
-from lib.utils import BM_DATA_DIR, BM_PRODUCT
+from lib.utils import BM_DATA_DIR, BM_PRODUCT, BM_VARIABLE
 
 
 def get_bm(path: str | Path | None = None):
-    path = Path(path) if path else BM_DATA_DIR / "preprocessed" / f"dataset-{BM_PRODUCT}.zarr"
+    path = Path(path) if path else BM_DATA_DIR / "preprocessed" / f"{BM_PRODUCT}-{BM_VARIABLE}.zarr"
 
     if not path.exists():
         raise ValueError(f"File does not exist: {path}")

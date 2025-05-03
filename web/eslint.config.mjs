83d6bafa90dcea606a7aa096fc1bc4196975d3e0
Federico Mantovani <fettemanto@gmail.com>
2025-05-03 12:07:11 +0200

    Map options

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -20,6 +20,7 @@ const eslintConfig = [
                 },
             ],
             "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-unused-vars": "off",
         },
     },
 ];

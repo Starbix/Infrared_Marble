e04a91f6f24eeca6b853411578ee39e7c63dd1b1
Federico Mantovani <fettemanto@gmail.com>
2025-05-04 13:38:58 +0200

    Add example sync endpoint with redis cache
    Adds an endpoint to retrieve some raster data from BM. Downloads data
    and stores it in redis cache once retrieved. Upon second request, will
    serve data from cache.
    
    Uses dependency injection to get process pool executor and redis client.
    Process pool executor is necessary as BM uses its own asyncio loop which
    would interfere with FastAPI's loop.

--- src/api/routers/comparison_router.py
+++ src/api/routers/comparison_router.py
@@ -1,22 +1,52 @@
-from datetime import datetime
+from concurrent.futures import ProcessPoolExecutor
+from datetime import date
+import io
 from typing import Annotated
-from fastapi.params import Path
+from fastapi import Depends, Response
 from fastapi.routing import APIRouter
 import geopandas
 import gzip
+import pickle
 
+from redis import Redis
+
+from api.dependencies import get_executor, get_redis_client
 from lib.download import bm_download
 from lib.utils import ADMIN_AREA_FILE_MAPPING, GEOJSON_ADMIN_KEY
 
 
-router = APIRouter(prefix="/compare/{date}/{adminId}", tags=["Compare"])
+router = APIRouter(prefix="/compare/{date}/{admin_id}", tags=["Compare"])
+
+
+def bm_geotiff_task(date: date, admin_id: str):
+    with gzip.open(ADMIN_AREA_FILE_MAPPING["50m"]) as f:
+        gdf = geopandas.read_file(f)
+    gdf_admin = gdf[gdf[GEOJSON_ADMIN_KEY] == admin_id]
+    bm_data = bm_download(gdf_admin, date)
+    return bm_data
 
 
 @router.get("/bm")
-async def get_bm_geotiff(date: Annotated[datetime, Path()], adminId: str):
-    with gzip.open(ADMIN_AREA_FILE_MAPPING["50m"]) as f:
-        gdf = geopandas.read_file(f)
-    gdf_admin = gdf[gdf[GEOJSON_ADMIN_KEY] == adminId]
-    bm_data = bm_download(gdf_admin, date.date())
-    print("Got BM data:", bm_data)
-    return bm_data
+async def get_bm_geotiff(
+    date: date,
+    admin_id: str,
+    executor: Annotated[ProcessPoolExecutor, Depends(get_executor)],
+    redis_client: Annotated[Redis, Depends(get_redis_client)],
+):
+    # Generate xarray
+    cache_key = f"bm:{date}:{admin_id}"
+    cached_data = redis_client.get(cache_key)
+    if cached_data:
+        raster = pickle.loads(cached_data)
+    else:
+        future = executor.submit(bm_geotiff_task, date, admin_id)
+        raster = future.result()
+        raster_serialized = pickle.dumps(raster)
+        redis_client.set(cache_key, raster_serialized)
+
+    # Convert xarray to GeoTIFF and return
+    with io.BytesIO() as buf:
+        raster["Gap_Filled_DNB_BRDF-Corrected_NTL"].rio.to_raster(buf, driver="GTiff", compress="LZW")
+        buf.seek(0)
+        content = buf.getvalue()
+    return Response(content=content, media_type="image/tiff")

d177c2e9da510a4bdcc62fbb4ccb4daed21248e3
Alexandre Iskandar <alexelgayar@gmail.com>
2025-05-07 00:45:15 +0200

    add cloud data endpoint

--- src/api/routers/statistics_router.py
+++ src/api/routers/statistics_router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, HTTPException
 
-from lib.loading import load_all_dates, load_country_meta
+from lib.loading import load_all_dates, load_country_meta, avail_dates
 from lib.utils import ADMIN_AREA_FILE_MAPPING
+from lib.misc import get_day_cloud_coverage
+
+import pycountry
 
 router = APIRouter(prefix="/statistics", tags=["Explore"])
 
@@ -36,3 +39,19 @@ async def get_region_dates(admin_id: str):
         raise HTTPException(status_code=404, detail="Admin area not found")
 
     return {"admin_id": admin_id, "dates": sorted(admin_data["date"].unique())}
+
+
+@router.get("/clouds/{admin_id}")
+async def get_stats_admin_area(admin_id : str):
+    """
+    Get cloud coverage for an admin area.
+    """
+    country_name = pycountry.countries.get(alpha_3=admin_id.upper()).name
+    dates = list(avail_dates(admin_id))
+    dates_clouds = {}
+    for date in dates:
+        lj,bm = get_day_cloud_coverage(date, country_name)
+        dates_clouds[date] = {"lj_cloud_coverage" : lj, "bm_cloud_coverage": bm}
+
+    return dates_clouds
+

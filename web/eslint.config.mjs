53ac58456034dd03340e3e391bd76b84320e5d89
Federico Mantovani <fettemanto@gmail.com>
2025-05-08 15:40:13 +0200

    Update ESLint config

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,6 +1,7 @@
-import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,9 +11,38 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.plugins("@typescript-eslint", "prettier", "import"),
+    ...compat.extends(
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+        "next/core-web-vitals",
+        "next/typescript",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+    ),
     {
         rules: {
+            "sort-imports": [
+                "warn",
+                {
+                    ignoreCase: false,
+                    ignoreDeclarationSort: true,
+                    ignoreMemberSort: false,
+                    memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+                    allowSeparatedGroups: true,
+                },
+            ],
+            "import/order": [
+                "warn",
+                {
+                    groups: ["builtin", "external", "internal", ["sibling", "parent"], "index", "unknown"],
+                    "newlines-between": "always",
+                    alphabetize: {
+                        order: "asc",
+                        caseInsensitive: true,
+                    },
+                },
+            ],
             "@typescript-eslint/no-empty-object-type": [
                 "error",
                 {

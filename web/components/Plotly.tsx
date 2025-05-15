df73dcb38ed112f8cd55c459ac9ba92598a541af
Federico Mantovani <fettemanto@gmail.com>
2025-05-15 20:11:17 +0200

    Clean up coverage map code

--- web/components/Plotly.tsx
+++ web/components/Plotly.tsx
@@ -1,8 +1,27 @@
+import { Box, CircularProgress, Typography } from "@mui/material";
 import dynamic from "next/dynamic";
 import { type PlotParams } from "react-plotly.js";
 
 // Dynamically load Plotly with SSR disabled
-const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
+const Plot = dynamic(() => import("react-plotly.js"), {
+  ssr: false,
+  loading: () => (
+    <Box
+      sx={{
+        width: 1,
+        minHeight: 300,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 2,
+      }}
+    >
+      <CircularProgress />
+      <Typography>Loading Plotly.js</Typography>
+    </Box>
+  ),
+});
 
 function SSRPlot(props: PlotParams) {
   return <Plot {...props} />;

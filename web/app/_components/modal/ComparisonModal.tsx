18ddff1b71961de57e6bc902adf915fbb3b8a106
Federico Mantovani <fettemanto@gmail.com>
2025-05-24 17:28:06 +0200

    Fix date display on web app

--- web/app/_components/modal/ComparisonModal.tsx
+++ web/app/_components/modal/ComparisonModal.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { Close as CloseIcon } from "@mui/icons-material";
-import { Box, Card, CardContent, CardHeader, IconButton, Modal, Skeleton, Typography } from "@mui/material";
-import dayjs from "dayjs";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Chip,
+  IconButton,
+  Modal,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
+import dayjs, { Dayjs } from "dayjs";
 import L from "leaflet";
 import useSWR from "swr";
 
@@ -38,12 +49,32 @@ const ComparisonModal: React.FC = (props) => {
     ) : (
       <span>NTL Comparison &mdash; {data.properties.name}</span>
     );
-  const CardSubHeader = () =>
-    isLoading ? (
+  const DateChip = ({ date, name, color }: { date: Dayjs; name: string; color: string }) => (
+    <Chip
+      size="small"
+      label={
+        <Stack direction="row" spacing={1}>
+          <Typography fontWeight="bold" fontSize={14}>
+            {name}
+          </Typography>
+          <Typography fontSize={14}>{date.format("ddd, DD MMM YYYY HH:mm")}</Typography>
+        </Stack>
+      }
+      sx={{ color, bgcolor: `color-mix(in srgb, ${color} 20%, transparent)`, mx: 1, px: 1 }}
+    />
+  );
+  const CardSubHeader = () => {
+    const bmDate = dayjs(`${date}T01:30`).add(1, "day");
+    const ljDate = dayjs(`${date}T22:30`);
+    return isLoading ? (
       <Skeleton variant="text" sx={{ fontSize: "0.7rem" }} />
     ) : error ? undefined : (
-      <span>Date: {dayjs(date).toString()}</span>
+      <span>
+        Imaging datetimes: <DateChip date={bmDate} name="Black Marble" color="#2092d4" />
+        <DateChip date={ljDate} name="LuoJia1-01" color="#d4209b" />
+      </span>
     );
+  };
 
   const layer = data ? L.geoJSON(data) : undefined;
 
